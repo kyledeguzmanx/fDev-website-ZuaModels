79e7f287869a5c575b4ca93281d244178b96759a
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-28 11:56:13 -0800

    new cusotm methods to fetch different web pages

--- react-hello/src/components/MainContent.jsx
+++ react-hello/src/components/MainContent.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 
+
 class MainContent extends Component {
     constructor(props) {
         super(props);
     }
     state = {  }
     render() { 
-        return (
+        const result = this.getHomepage();
+        return result;
+    }
+
+    getHomepage(){
+        return(
             <main>
-                
+            <h1>Homepage</h1>
             </main>
-          );
+        );
+    }
+    getMens(){
+        return(
+            <main>
+            <h1>Mens</h1>
+            </main>
+        );
+    }
+    getWoens(){
+        return(
+            <main>
+            <h1>Womens</h1>
+            </main>
+        );
     }
 }
  
