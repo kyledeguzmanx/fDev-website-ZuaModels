b6d865484fde4c427c6c28ca3adad0a9970cbdd7
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-27 23:20:35 -0800

    imported and rendered footer

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-
+ReactDOM.render(<Navbar/>, document.getElementById("root"));
 ReactDOM.render(<Navbar/>, document.getElementById("root"));
