1852a666cbbfefc9ed4f5f36d7c5d07b744e1c63
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-27 23:48:12 -0800

    added social btns to DOM

--- react-hello/src/components/Footer.jsx
+++ react-hello/src/components/Footer.jsx
@@ -5,12 +5,21 @@ class Footer extends Component {
         super(props);
     }
     state = { 
-        imageURL: "./images/logo.png"
+        imageURL: "./images/logo.png",
+        instagramIconPath: "./images/instagram.svg", 
+        facebookIconPath: "./images/facebook.svg", 
+        twitterIconPath: "./images/twitter.svg"
      }
     render() { 
         return ( 
             <footer>
                 <img src={this.state.imageURL}/>
+                <a href="tel:+13234367700">+1 323.436.7700</a>
+                <div>
+                    <img src={this.state.instagramIconPath} alt="" />
+                    <img src={this.state.facebookIconPath} alt="" />
+                    <img src={this.state.twitterIconPath} alt="" />
+                </div>
             </footer>
          );
     }
