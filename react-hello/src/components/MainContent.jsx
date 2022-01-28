98369384a4e2e482cc0ce8b17b2b28241d6fc9e8
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-28 11:16:20 -0800

    new main component

---
+++ react-hello/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MainContent extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (
+            <main>
+                
+            </main>
+          );
+    }
+}
+ 
+export default MainContent;
--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -6,12 +6,15 @@ import './index.css';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+
 
 const App = () => {
 
   return (
     <React.Fragment>
       <Navbar/>
+      <MainContent/>
       <Footer/>
     </React.Fragment>
   )
