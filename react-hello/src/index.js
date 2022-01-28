19c0419c1f16ec56940a462b879c7e4c09a1f829
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-27 21:55:53 -0800

    installed react app

---
+++ react-hello/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const helloWorld = <h1>Hello World</h1>;
+
+ReactDOM.render(helloWorld, document.getElementById("root"));
