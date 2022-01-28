8d060fa872b800af88f86a37557774ba5900c3e8
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-28 13:13:44 -0800

    new timer, changing banner

--- react-hello/src/components/MainContent.jsx
+++ react-hello/src/components/MainContent.jsx
@@ -4,11 +4,40 @@ import React, { Component } from 'react';
 class MainContent extends Component {
     constructor(props) {
         super(props);
+
+        const bannerPath = [
+            "./images/Homepage/Fenty.jpg",
+            "./images/Homepage/Dior.jpg"
+        ]
+        this.state = { 
+            bannerPath,
+            bannerImgPath: 0,
+            currentPage : "Home"
+         }
     }
-    state = { 
-        currentPage : "Home",
-        bannerImgPath: "./images/Homepage/Fenty.jpg"
-     }
+    
+
+    componentDidMount() {
+      this.interval = setInterval(() => this.changeImage(), 4000);
+    }
+    componentWillUnmount() {
+        if (this.interval) {
+          clearInterval(this.interval); 
+        }
+    }
+    changeImage() {
+        let newCurrentImg = 0;
+        const bannerPath = this.state.bannerPath;
+        const bannerImgPath = this.state.bannerImgPath;
+        const noOfImages = bannerPath.length;
+        
+        if (bannerImgPath !== noOfImages - 1) {
+          newCurrentImg = bannerImgPath + 1;
+        }
+  
+        this.setState({bannerImgPath: newCurrentImg});
+        console.log(this.state.bannerImgPath);
+    }
     render() { 
         
         const result = this.getMainContent();
@@ -21,7 +50,7 @@ class MainContent extends Component {
                 <main>
                 <h1>Home</h1>
                 <div id="home-banner">
-                    <img src={this.state.bannerImgPath}></img>
+                    <img src={this.state.bannerPath[this.state.bannerImgPath]}></img>
                 </div>
                 </main>
             );
