83a34c44a51e69d597bfbbf22d54a936af4f54c7
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-27 23:17:22 -0800

    refactoring folders

---
+++ react-hello/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = { 
+        imageURL: "./images/logo.png"
+     }
+    render() { 
+        return ( 
+            <nav>
+                <div id="nav-logo">
+                    <img src={this.state.imageURL}></img>
+                </div>
+                <div id="nav-link">
+                    <ul>
+                        <li><a href="#">Women</a></li>
+                        <li><a href="#">Men</a></li>
+                        <li><a href="#">Juniors</a></li>
+                    </ul>
+                    <div>
+                        <a href=""> &#9776;</a>
+                    </div>
+                </div>
+            </nav>
+         );
+    }
+}
+ 
+export default Navbar;
--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 
 
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 
 
 ReactDOM.render(<Navbar/>, document.getElementById("root"));
