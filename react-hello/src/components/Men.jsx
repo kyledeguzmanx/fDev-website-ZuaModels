654bcccd2c856db540c1da58b76508d403250ccc
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-29 23:18:27 -0800

    basic boiler template

---
+++ react-hello/src/components/Men.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Men extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Men;
---
+++ react-hello/src/components/Women.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Women extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Women;
