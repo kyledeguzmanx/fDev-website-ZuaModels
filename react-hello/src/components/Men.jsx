8f06b39787b89c1e74ff8a18e7d61f340bcf1425
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-01 17:15:41 -0800

    replaced flexbox for inline blocks. This way, as the screen narrows, the last picture will drop to the next column

--- react-hello/src/components/Men.jsx
+++ react-hello/src/components/Men.jsx
@@ -85,6 +85,193 @@ class Men extends Component {
     render() { 
         return (
             <main>
+                <section id="main-gallery">
+                    <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[0].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[0].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[1].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[1].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[2].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[2].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[3].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[3].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[4].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[4].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[5].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[5].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[6].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[6].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[7].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[7].name}</p>
+                        </div> 
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[8].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[8].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[9].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[9].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[10].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[10].name}</p>
+                        </div>
+                        <div className='card-container'> 
+                            <div class="img-container">
+                                <img src={this.state.models[11].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[11].name}</p>
+                        </div>
+                        <div className='card-container'> 
+                            <div className="img-container">
+                                <img src={this.state.models[12].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[8].name}</p>
+                        </div>
+                        <div className='card-container'> 
+                            <div className="img-container">
+                                <img src={this.state.models[13].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[8].name}</p>
+                        </div>
+                </section>
+            </main>
+          );
+    }
+}
+ 
+export default Men;
+
+
+
+
+
+
+
+//DRAFT ONE
+/*import React, { Component } from 'react';
+
+class Men extends Component {
+    constructor(props) {
+        super(props);
+
+        const models = [
+            {
+                name: "Amerigo Valenti",
+                imgProfile:  "./images/Models/AValenti-PFP.jpg",
+                imgShoot:  "./images/Models/AValenti-MS.jpg"
+            },
+            {
+                name: "Anwar Hadid",
+                imgProfile:  "./images/Models/AHadid-PFP.jpg",
+                imgShoot:  "./images/Models/AHadid-MS.jpg"
+            },
+            {
+                name: "Alton Mason",
+                imgProfile: "./images/Models/AMason-PFP.jpg",
+                imgShoot: "./images/Models/AMason-MS.jpg"
+            },
+            {
+                name: "Bom Chan Lee",
+                imgProfile: "./images/Models/BLee-PFP.jpg",
+                imgShoot: "./images/Models/BLee-MS.jpg"
+            },
+            {
+                name: "Charlie Knepper",
+                imgProfile: "./images/Models/CKnepper-PFP.jpg",
+                imgShoot: "./images/Models/CKnepper-MS.jpg"
+            },
+            {
+                name: "Hudson Taylor",
+                imgProfile: "./images/Models/HTaylor-PFP.jpg",
+                imgShoot: "./images/Models/HTaylor-MS.jpg"
+            },
+            {
+                name: "Joe Rodonis",
+                imgProfile: "./images/Models/JRodonis-PFP.jpg",
+                imgShoot: "./images/Models/JRodonis-MS.jpg"
+            },
+            {
+                name: "Kohei",
+                imgProfile: "./images/Models/Kohei-PFP.jpg",
+                imgShoot: "./images/Models/Kohei-MS.jpg"
+            },
+            {
+                name: "Niyo Malik",
+                imgProfile: "./images/Models/NMalik-PFP.jpg",
+                imgShoot: "./images/Models/NMalik-MS.jpg"
+            },
+            {
+                name: "Oli Lacey",
+                imgProfile: "./images/Models/OLacey-PFP.png",
+                imgShoot: "./images/Models/OLacey-MS.jpg"
+            },
+            {
+                name: "Peter Dupont",
+                imgProfile: "./images/Models/PDupont-PFP.jpg",
+                imgShoot: "./images/Models/PDupont-MS.jpg"
+            },
+            {
+                name: "Tyson Ballou",
+                imgProfile: "./images/Models/TBallou-PFP.jpg",
+                imgShoot: "./images/Models/TBallou-MS.jpeg"
+            },
+            {
+                name: "Victor Norlander",
+                imgProfile: "./images/Models/VNorlander-PFP.jpg",
+                imgShoot: "./images/Models/VNorlander-MS.jpg"
+            },
+            {
+                name: "Wang Chen Ming",
+                imgProfile: "./images/Models/WMing-PFP.jpg",
+                imgShoot: "./images/Models/WMing-MS.jpg"
+            }
+        ]
+
+        this.state = {
+            models
+        }
+    }
+
+    render() { 
+        return (
+            <main>
                  <div className="contain-four">
                     <div className="contain-two">
                         <div className='card-container'>
@@ -196,4 +383,4 @@ class Men extends Component {
     }
 }
  
-export default Men;
+export default Men;*/
