fbb2716dd6c1df6ed2cea315ee91d13e1d3ed56e
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-01 20:51:14 -0800

    added new model object list

---
+++ react-hello/src/components/Women.jsx
@@ -0,0 +1,179 @@
+import React, { Component } from 'react';
+
+class Women extends Component {
+    constructor(props) {
+        super(props);
+
+        const models = [
+            {
+                name: "Alek Wek",
+                imgProfile:  "./images/Models/AWek-PFP.jpg",
+                imgShoot:  "./images/Models/AWek-MS.jpg"
+            },
+            {
+                name: "Alexis Ruby",
+                imgProfile:  "./images/Models/ARuby-PFP.jpg",
+                imgShoot:  "./images/Models/ARuby-MS.jpg"
+            },
+            {
+                name: "alyssah ali",
+                imgProfile: "./images/Models/AAli-PFP.jpg",
+                imgShoot: "./images/Models/AMaAAlison-MS.jpg"
+            },
+            {
+                name: "barbara palvin",
+                imgProfile: "./images/Models/BPalvin-PFP.jpg",
+                imgShoot: "./images/Models/BPalvin-MS.jpg"
+            },
+            {
+                name: "bella hadid",
+                imgProfile: "./images/Models/BHadid-PFP.jpg",
+                imgShoot: "./images/Models/BHadid-MS.jpg"
+            },
+            {
+                name: "chanel iman",
+                imgProfile: "./images/Models/CIman-PFP.jpg",
+                imgShoot: "./images/Models/CIman-MS.jpg"
+            },
+            {
+                name: "charlee fraser",
+                imgProfile: "./images/Models/CFraser-PFP.jpg",
+                imgShoot: "./images/Models/CFraser-MS.jpg"
+            },
+            {
+                name: "Daria Werbowy",
+                imgProfile: "./images/Models/DWerbowy-PFP.jpg",
+                imgShoot: "./images/Models/DWerbowy-MS.jpg"
+            },
+            {
+                name: "Du Juan",
+                imgProfile: "./images/Models/DJuan-PFP.jpg",
+                imgShoot: "./images/Models/DJuan-MS.jpg"
+            },
+            {
+                name: "Elsa hosk",
+                imgProfile: "./images/Models/EHosk-PFP.jpg",
+                imgShoot: "./images/Models/EHosk-MS.jpg"
+            },
+            {
+                name: "ellie thumann",
+                imgProfile: "./images/Models/EThumann-PFP.jpg",
+                imgShoot: "./images/Models/EThumann-MS.jpg"
+            },
+            {
+                name: "georgia fowler",
+                imgProfile: "./images/Models/GFowler-PFP.jpg",
+                imgShoot: "./images/Models/GFowler-MS.jpg"
+            },
+            {
+                name: "Hyunji Shin",
+                imgProfile: "./images/Models/HShin-PFP.jpg",
+                imgShoot: "./images/Models/HShin-MS.jpg"
+            },
+            {
+                name: "Jennifer Atilemile",
+                imgProfile: "./images/Models/JAtilemile-PFP.jpg",
+                imgShoot: "./images/Models/JAtilemile-MS.jpg"
+            }
+        ]
+
+        this.state = {
+            models
+        }
+    }
+
+    render() { 
+        return (
+            <main>
+                <section id="main-gallery">
+                    <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[0].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[0].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[1].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[1].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[2].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[2].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[3].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[3].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[4].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[4].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[5].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[5].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[6].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[6].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[7].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[7].name}</p>
+                        </div> 
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[8].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[8].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[9].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[9].name}</p>
+                        </div>
+                        <div className='card-container'>
+                            <div className="img-container">
+                                <img src={this.state.models[10].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[10].name}</p>
+                        </div>
+                        <div className='card-container'> 
+                            <div class="img-container">
+                                <img src={this.state.models[11].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[11].name}</p>
+                        </div>
+                        <div className='card-container'> 
+                            <div className="img-container">
+                                <img src={this.state.models[12].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[12].name}</p>
+                        </div>
+                        <div className='card-container'> 
+                            <div className="img-container">
+                                <img src={this.state.models[13].imgProfile}></img>
+                            </div>
+                            <p>{this.state.models[13].name}</p>
+                        </div>
+                </section>
+            </main>
+          );
+    }
+}
+ 
+export default Women;
